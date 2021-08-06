d1611b0830f554beed5fda4c7d3a85823e49d38d
Ranjot <ranjotdhoofer@gmail.com>
2021-08-06 12:58:08 +0100

    Create a block class and initialise a 2D array of blocks.
    A block can have a colour, position and size. The position is determined through the row and column parameters.

--- Breakout Game/Form1.cs
+++ Breakout Game/Form1.cs
@@ -15,6 +15,31 @@ namespace Breakout_Game
         public Breakout()
         {
             InitializeComponent();
+
+            Block[,] blocks = new Block[8, 15];
+
+            //creates 8 rows with 15 columns
+            for(int i = 0; i<8; i++)
+            {
+                for(int j = 0; j<15; j++)
+                {
+                    blocks[i, j] = new Block(i, j);
+                }
+            }
+        }
+    }
+    public class Block
+    {
+        public RectangleF block { get; private set; }
+        public Color Colour { get; private set; }
+        public bool hit { get; private set; }
+
+        public Block(int row, int column)
+        {
+            int RowMultiplier = 10;
+            int ColumnMultiplier = 10;
+            hit = false;
+            block = new RectangleF(new PointF(row * RowMultiplier, column * ColumnMultiplier), new SizeF(10,10));
         }
     }
 }
